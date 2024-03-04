55bce17f5b0231526ffaf5fd0a87707fe98f5465
Ignacio Belitzky <ignabelitzky@gmail.com>
2024-03-04 16:21:55 -0300

    Minor changes in the demo

--- misc/mbox_demo.cpp
+++ misc/mbox_demo.cpp
@@ -6,9 +6,9 @@ int main() {
     std::cout << "Press 1 if you want a message box with a title, or 2 if you don't want a title: ";
     std::cin >> choice;
     std::string message = "This is a message box...\n\n...with a message!\n\nUse the arrow keys to select an option and press Enter to confirm.";
-    std::vector<std::string> options = {"Option 1", "Option 2", "Option 3"};
+    std::vector<std::string> options = {"Start new game", "Load game", "Close"};
     if(choice == 1) {
-        std::string title = "My message box";
+        std::string title = " [My message box] ";
         try {
             MessageBox* mbox = new MessageBox(title, message, options);
             std::string selection = mbox->show();
